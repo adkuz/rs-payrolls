8df1457466691b746736c8c499c42b7342f59782
Alexander Kuznetsov <17350870+adkuz@users.noreply.github>
2024-12-09 22:44:04 +0100

    feat: update consts 2024

--- librstax/consts.py
+++ librstax/consts.py
@@ -1,6 +1,11 @@
 
 CONSTS = {
     "2023": {
-        "Non-taxable amount": 21712.00
+        "Non-taxable amount": 21712.00,
+        "Base for contributions": 500360.00,
+    },
+    "2024": {
+        "Non-taxable amount": 25000.00,
+        "Base for contributions": 573470.00,
     }
 }
